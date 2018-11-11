ebeaca0def82e1f0a34446e93a3399a6dca865ec
mhd0528 <1400012862@pku.edu.cn>
2018-11-11 17:06:32 -0500

    add ArgReader

---
+++ src/controller/ArgReader.java
@@ -0,0 +1,91 @@
+package controller;
+import java.io.*;
+import java.util.*;
+import model.*;
+
+public class ArgReader {
+	private static ArgReader instance;
+	//private PeerInfo myInfo;
+	//private List<PeerInfo> peerInfoList;
+	private int NumberOfPreferredNeighbors;
+	private int UnchokingInterval;
+	private int OptimisticUnchokingInterval;
+	private String fileName;
+	private int fileSize;
+	private int pieceSize;
+	private ArgReader() {
+		try {
+			FileReader reader = new FileReader("./Common.cfg");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            //peerInfoList = new ArrayList<>();
+            String str = null;
+            while((str = bufferedReader.readLine()) != null) {
+            	String[] netArgs = str.split(" ");
+            	if (netArgs[0].equals("NumberOfPreferredNeighbors")){
+            		NumberOfPreferredNeighbors = Integer.parseInt(netArgs[1]);
+            	}
+            	else if(netArgs[0].equals("UnchokingInterval")){
+            		UnchokingInterval = Integer.parseInt(netArgs[1]);
+            	}
+            	else if(netArgs[0].equals("OptimisticUnchokingInterval")){
+            		OptimisticUnchokingInterval = Integer.parseInt(netArgs[1]);
+            	}
+            	else if(netArgs[0].equals("FileName")){
+            		fileName = netArgs[1];
+            	}
+            	else if(netArgs[0].equals("FileSize")){
+            		fileSize = Integer.parseInt(netArgs[1]);
+            	}
+            	else{
+            		pieceSize = Integer.parseInt(netArgs[1]);
+            	}
+            	
+            }
+            
+            bufferedReader.close();
+            reader.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static ArgReader getInstance() {
+		if (instance == null) {
+			instance = new ArgReader();
+		}
+		return instance;
+	}
+	
+	public int getNumberOfPreferredNeighbors(){
+		return NumberOfPreferredNeighbors;
+	}
+	
+	public int getUnchokingInterval(){
+		return UnchokingInterval;
+	}
+	
+	public int getOptimisticUnchokingInterval(){
+		return OptimisticUnchokingInterval;
+	}
+
+	public String getfileName(){
+		return fileName;
+	}
+	
+	public int getfileSize(){
+		return fileSize;
+	}
+	
+	public int getpieceSize(){
+		return pieceSize;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(ArgReader.getInstance().getNumberOfPreferredNeighbors());
+		System.out.println(ArgReader.getInstance().getUnchokingInterval());
+		System.out.println(ArgReader.getInstance().getOptimisticUnchokingInterval());
+		System.out.println(ArgReader.getInstance().getfileName());
+		System.out.println(ArgReader.getInstance().getfileSize());
+		System.out.println(ArgReader.getInstance().getpieceSize());
+	}
+}
