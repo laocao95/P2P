25a29b2b4816f91d40a624a5eb47048eba07d260
mhd0528 <1400012862@pku.edu.cn>
2018-11-11 22:02:30 -0500

    implement funcs with bitfield messages

--- src/controller/BitfieldManager.java
+++ src/controller/BitfieldManager.java
@@ -5,38 +5,59 @@ import model.*;
 
 public class BitfieldManager {
 	private static BitfieldManager instance;
-	
-	private int peerNum = PeerInfoManager.getInstance().getSize();
 	private int pieceSize = ArgReader.getInstance().getfileSize()/ArgReader.getInstance().getfileSize();
-	private boolean[] ownBitfield = new boolean[pieceSize];
-	//private boolean[][] othersBitfield = new boolean[peerNum+1][pieceSize+1] ;
-	HashMap<Integer, boolean[]> othersBitfield = new HashMap<>();
+	HashMap<PeerInfo, boolean[]> bitFields = new HashMap<>();
+	private BitfieldManager(){
+		//PeerInfoManagerPeer
+		for (PeerInfo peer : PeerInfoManager.getInstance().getPeers()){
+			boolean singlePeer[] = new boolean[pieceSize];
+			if(peer.getHasFile()){
+				Arrays.fill(singlePeer, true);
+				bitFields.put(peer, singlePeer);
+			}
+			else{
+				Arrays.fill(singlePeer, false);
+				bitFields.put(peer, singlePeer);
+			}
+		}
+	}
 	public static BitfieldManager getInstance(){
 		if (instance == null) {
 			instance = new BitfieldManager();
 		}
 		return instance;
 	}
-	/*
-	public boolean[] getmyBitfield(){
-		return ownBitfield;
-	}
-	
-	public boolean[] getOthersBitfield(int peerID){
-		//return othersBitfield[peerID];
-		return null;
-	}
-	*/
-	public void updateMyBitfield(int pieceNum){
-		ownBitfield[pieceNum] = true;
-	}
+
 	//for receive have message
-	public void updateOtherBitfield(int peerID, int pieceNum){
+	synchronized public void updateBitfield(int peerInfo, int pieceNum){
 		//othersBitfield[peerID][pieceNum] = true;
+		boolean[] tmp = bitFields.get(peerInfo);
+		tmp[pieceNum] = true;
 	}
 	
-	public int compareAndchoose(int otherID){
-		return 1;
+	public int compareAndchoose(PeerInfo peerInfo){
+		//compare if there is interested pieces
+		boolean[] myBitField = bitFields.get(PeerInfoManager.getInstance().getMyInfo());
+		boolean[] destBitField = bitFields.get(peerInfo);
+		int[] diff = new int[pieceSize];
+		boolean flag = false;
+		Arrays.fill(diff, 0);
+		for (int i = 0; i < pieceSize; i++){
+			if(myBitField[i] == false && destBitField[i] == true){
+				diff[i] = i;
+				flag = true;
+			}
+		}
+		//no interested pieces
+		if (flag == false){
+			return -1;
+		}
+		else {
+			//random choose a file piece
+			int want = new Random().nextInt(diff.length);
+			return want;
+		}
+		
 	}
 }
 
